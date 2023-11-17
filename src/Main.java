ee104c84a69f83600e11036dab384846180c80d5
Siddhant <siddhantmishra725@gmail.com>
2023-11-17 15:03:06 +0530

    Reafactored the code for calculator

--- src/Main.java
+++ src/Main.java
@@ -20,15 +20,18 @@ import StrategyPlusFactorDesignPattern.PaymentProcessor;
 import java.security.Timestamp;
 import java.util.Timer;
 import Calculator.ScientificCalculator;
+import Calculator.CalculatorFactory;
+import Calculator.CalculatorType;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        Calculator calculator1 = new Calculator();
+        CalculatorFactory calculatorFactory = new CalculatorFactory();
+        Calculator calculator1 = calculatorFactory.getCalculator(CalculatorType.NORMAL);
 
         System.out.println(calculator1.add(2, 3));
         System.out.println(calculator1.sub(5, 2));
 
-        Calculator calculator2 = new ScientificCalculator();
+        Calculator calculator2 = calculatorFactory.getCalculator(CalculatorType.SCIENTIFIC);
         System.out.println(calculator2.add(2, 3));
         System.out.println(calculator2.sub(5, 2));
         System.out.println(((ScientificCalculator) calculator2).modulo(3, 5));
